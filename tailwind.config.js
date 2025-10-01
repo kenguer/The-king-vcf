4b1354e79949cb214baa78b4264340427ff87456
kenguer <kenguerflore@gmail.com>
2025-10-01 10:05:53 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgmain: "#0D1117",
+        bgsoft: "#161B22",
+        accent: "#2FD771"
+      },
+      borderRadius: {
+        xl2: "1.25rem"
+      }
+    }
+  },
+  plugins: []
+};
